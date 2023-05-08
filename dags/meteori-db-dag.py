d7afdbcbc7a3214be9754d653e0dbfb034ce1795
Levi Carbellido <tomas.carbellido@gmail.com>
2023-05-08 03:56:58 -0600

    refactor: use taskflow API with DAG decorator

--- dags/meteori-db-dag.py
+++ dags/meteori-db-dag.py
@@ -1,11 +1,11 @@
-import logging
 from datetime import datetime, timedelta
+import logging
 import re
 import requests
 import sqlite3
 
-from airflow import DAG
-from airflow.decorators import task
+from airflow.decorators import dag, task
+
 
 default_args = {
     'owner': 'epiclevi',
@@ -13,96 +13,57 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
+TaskFeeling = {
+    'BAD_SQL': -1,
+    'OK': 0
+}
+
 log = logging.getLogger(__name__)
 
-with DAG(
-    dag_id='meteori-db-dag-1',
-    description='DAG for meteori-db, part 1',
+
+@dag(
+    dag_id='meteori-db-dag-2',
+    description='DAG for meteori-db, part 2',
     start_date=datetime(2023, 5, 5),
-    schedule_interval=timedelta(hours=1),
-):
-    # [START extract_task]
-    # TODO: read the links from an external src, e.g. a JSON file as a Dataset
-    default_url_list = ['https://www.meteored.mx/ciudad-de-mexico/historico',
-                        'https://www.meteored.mx/monterrey/historico',
-                        'https://www.meteored.mx/merida/historico',
-                        'https://www.meteored.mx/wakanda/historico']
-
-    @task(task_id='extract_task')
-    def extract_task(url_list: 'list[str]' = default_url_list, **context):
-        task_instance = context['task_instance']
-        city_data_list: 'list[dict]' = []
-        run_id = f'{datetime.now().isoformat()}'
-
-        for url in url_list:
-            try:
-                response = requests.get(url)
-                city_data_list.append(parse_response(run_id, response))
-                response.raise_for_status()
-                log.info(f'Received data from {url}')
-
-            except Exception as e:
-                log.error(f'Error scraping {url}): {str(e)}')
-
-        task_instance.xcom_push(key='city_data_list', value=city_data_list)
-
-    def parse_response(run_id: str, response: requests.Response = None) -> 'dict':
-        city_name = response.url.split('/')[-2]
-        url = response.url
-        city_dict = {
-            'run_id': run_id,
-            'city': city_name,
-            'url': url,
-            'status_code': response.status_code
-        }
-
-        if response.status_code == 200:
-            city_dict.update({
-                'distance': extract('distance', response),
-                'last_updated': extract('last_updated', response),
-                'temperature': extract('temperature', response),
-                'humidity': extract('humidity', response),
-            })
-
-        return city_dict
-
-    def extract(pattern: str, response: requests.Response):
-        text = response.text
-        match: re.Match = None
-
-        if pattern == 'distance':
-            match = re.search(r'<span id="dist_cant">([\d.]+)km<\/span>', text)
-        elif pattern == 'last_updated':
-            match = re.search(r'<span id="fecha_act_dato">(.+?)<\/span>', text)
-        elif pattern == 'temperature':
-            match = re.search(r'<span id="ult_dato_temp">(\d+)<\/span>', text)
-        elif pattern == 'humidity':
-            match = re.search(
-                r'<span id="ult_dato_hum">([\d.]+)<\/span>', text)
-        else:
-            raise ValueError(f'Unknown pattern {pattern}')
-
-        if match:
-            value = match.group(1)
-            return value
-        else:
-            log.warn(f'No match found for {pattern} in {response.url}')
+    schedule_interval='@hourly'
+)
+def meteori_db_dag_taskflow():
+    # [START drop_tables_task]
+    @task()
+    def drop_tables_task(skip=True):
+        if skip:
             return None
 
-    task1 = extract_task()
-    # [END extract_task]
+        conn = sqlite3.connect('meteori.db')
+        cursor = conn.cursor()
 
-    # [START tmp_drop_tables_task]
-    @task(task_id='tmp_drop_tables_task')
-    def tmp_drop_tables_task():
-        pass
+        try:
+            drop_table_query = 'DROP TABLE IF EXISTS meteored_readings;'
+            cursor.execute(drop_table_query)
 
-    tmp_task = tmp_drop_tables_task()
-    # [END tmp_drop_tables_task]
+            drop_table_query = 'DROP TABLE IF EXISTS cities;'
+            cursor.execute(drop_table_query)
+
+            drop_table_query = 'DROP TABLE IF EXISTS responses;'
+            cursor.execute(drop_table_query)
+
+            conn.commit()
+        except sqlite3.Error as e:
+            log.error(f'Error dropping tables: {e}')
+            return TaskFeeling['BAD_SQL']
+        finally:
+            cursor.close()
+            conn.close()
+
+        return TaskFeeling['OK']
+    # [END drop_tables_task]
 
     # [START create_db_task]
-    @task(task_id='create_db_task')
-    def create_db_task():
+    @task()
+    def create_db_task(drop_tables_feeling: int):
+        if drop_tables_feeling == TaskFeeling['OK']:
+            log.debug('drop_tables_task is feeling OK.')
+
         conn = sqlite3.connect('meteori.db')
         cursor = conn.cursor()
 
@@ -144,19 +105,93 @@ with DAG(
         conn.commit()
         cursor.close()
         conn.close()
-
-    task2 = create_db_task()
+        return TaskFeeling['OK']
     # [END create_db_task]
 
+    default_url_list = [
+        'https://www.meteored.mx/ciudad-de-mexico/historico',
+        'https://www.meteored.mx/monterrey/historico',
+        'https://www.meteored.mx/merida/historico',
+        'https://www.meteored.mx/wakanda/historico'
+    ]
+
+    # [START extract_task]
+    @task()
+    def extract_task(url_list: 'list[str]' = default_url_list):
+        city_data_list: 'list[dict]' = []
+        run_id = f'{datetime.now().isoformat()}'
+
+        for url in url_list:
+            try:
+                response = requests.get(url)
+                city_data_list.append(parse_response(run_id, response))
+                response.raise_for_status()
+                log.info(f'Received data from {url}')
+
+            except Exception as e:
+                log.error(f'Error scraping {url}): {str(e)}')
+
+        return city_data_list
+
+    def parse_response(run_id: str, response: requests.Response = None) -> 'dict':
+        city_name = response.url.split('/')[-2]
+        url = response.url
+        city_dict = {
+            'run_id': run_id,
+            'city': city_name,
+            'url': url,
+            'status_code': response.status_code
+        }
+
+        if response.status_code == 200:
+            try:
+                city_dict.update({
+                    'distance': extract('distance', response),
+                    'last_updated': extract('last_updated', response),
+                    'temperature': extract('temperature', response),
+                    'humidity': extract('humidity', response),
+                })
+            except ValueError as e:
+                log.error(f'This should not have happened bro: {e}')
+                return None
+
+        return city_dict
+
+    def extract(pattern: str, response: requests.Response):
+        text = response.text
+        match: re.Match = None
+
+        if pattern == 'distance':
+            match = re.search(r'<span id="dist_cant">([\d.]+)km<\/span>', text)
+        elif pattern == 'last_updated':
+            match = re.search(r'<span id="fecha_act_dato">(.+?)<\/span>', text)
+        elif pattern == 'temperature':
+            match = re.search(r'<span id="ult_dato_temp">(\d+)<\/span>', text)
+        elif pattern == 'humidity':
+            match = re.search(
+                r'<span id="ult_dato_hum">([\d.]+)<\/span>', text)
+        else:
+            raise ValueError(f'Unknown pattern {pattern}')
+
+        if match:
+            value = match.group(1)
+            return value
+        else:
+            log.warn(f'No match found for {pattern} in {response.url}')
+            return None
+    # [END extract_task]
+
+    # [START transform_task]
+    @task()
+    def transform_task(city_data_list: 'list[dict]'):
+        return city_data_list
+    # [END transform_task]
+
     # [START populate_db_task]
-    @task(task_id='populate_db_task')
-    def populate_db_task(**context):
-        # TODO: use a Dataset instead of XComs.
-        # XComs are not meant to pass large values,
-        # so this is not ideal when scraping a large number of cities
-        task_instance = context['task_instance']
-        city_data_list = task_instance.xcom_pull(
-            task_ids='extract_task', key='city_data_list')
+    @task()
+    def load_task(city_data_list: 'list[dict]', create_db_feeling: int):
+        if create_db_feeling == TaskFeeling['OK']:
+            log.debug(f'create_db_task is feeling OK.')
 
         conn = sqlite3.connect('meteori.db')
         cursor = conn.cursor()
@@ -179,8 +214,13 @@ with DAG(
 
         # Commit the changes to the database
         conn.commit()
-
-    task3 = populate_db_task()
     # [END populate_db_task]
 
-    task1 >> task2 >> task3
+    drop_tables_feeling = drop_tables_task(skip=False)
+    create_db_feeling = create_db_task(drop_tables_feeling)
+    city_data_list = extract_task()
+    city_data_list = transform_task(city_data_list)
+    load_task(city_data_list, create_db_feeling)
+
+
+meteori_db_dag_taskflow()
